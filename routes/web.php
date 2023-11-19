f8a82013a268638abf01792a73a477eb7371a9d7
hy-HA <heeyoung-ha@naver.com>
2023-11-19 20:54:23 +0900

    crud

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\BlogController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -17,6 +18,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('blog',BlogController::class)
+    ->only(['index', 'store', 'edit', 'update','destroy'])
+    ->middleware(['auth','verified']);
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
